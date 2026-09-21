3f669fd5c13c36152cd3169bbd5f3c9fa600b36a
ivaanrodriiguezzz <ivanrodriguezgomez-landero.guadalupe@alumnado.fundacionloyola.net>
2026-09-21 08:26:27 +0000

    Cambio

---
+++ Asignaturas2.php
@@ -0,0 +1,12 @@
+<?php
+    $horario=[
+        0 => ['Hora','Lunes','Martes','Miércoles','Jueves','Viernes'],
+        1 => [1,'FOL','CLIENTE','FOL','SERVIDOR','OPT2I'],
+        2 => [2,'SERVIDOR','CLIENTE','CLIENTE','SERVIDOR','OPT2A'],
+        3 => [3,'SERVIDOR','SERVIDOR','CLIENTE','SERVIDOR','DASP'],
+        4 => [4,'PIMOD','SERVIDOR','SERVIDOR','SOSTENIBILIDAD','SERVIDOR'],
+        5 => [5,'DESPLIEGUE','PIMOD','DESPLIEGUE','OPT1','SERVIDOR'],
+        6 => [6,'CLIENTE','DESPLIEGUE','DESPLIEGUE','FOL','TUTORIA'],
+        7 => [7,'CLIENTE','','','',''],
+    ];
+?>
